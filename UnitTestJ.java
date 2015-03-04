05b7d2915cd93d85425498080170a828d67e6c3f
Hussnain Cheema <hussnain_93@hotmail.com>
2015-03-04 21:03:14 +0500

    Create UnitTestJ.java

---
+++ UnitTestJ.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class UnitTestJ {
+	public static void main(String[] args) throws IOException {
+	  	Jserver ser = new Jserver();
+	  	ser.server();
+	  	}
+
+}
