39e6aa9f77910312abdd794d544769497f2fb694
Hussnain Cheema <hussnain_93@hotmail.com>
2015-03-04 21:04:33 +0500

    Create UnitTestJ1.java

---
+++ UnitTestJ1.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+public class UnitTestJ1 {
+	public static void main(String[] argv) throws Exception  {
+	  	Jclient cli = new Jclient();
+	  	cli.client();
+   
+  }
+
+}
